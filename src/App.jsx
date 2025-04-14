7651b54432d8bdaa9e51bafe86468dc403d60c23
ManyaSahai <manyasahai9548@gmail.com>
2025-04-14 19:51:29 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import Contact from "./pages/ContactPage/Contact.jsx";
 import Offers from "./pages/OffersPage/Offers.jsx";
 import SuperAdminDashboard from "./pages/SuperAdminDashboard/SuperAdminDashboard.jsx";
 import SellerDashboard from "./pages/SellerDashboard/Seller.jsx";
+import RegionalAdmin from "./pages/RegionalAdmin/RegionalAdmin.jsx";
 import Products from "./components/SuperAdminDashboard/Products.jsx";
 import Users from "./components/SuperAdminDashboard/Users.jsx";
 import Reports from "./components/SuperAdminDashboard/Reports.jsx";
-import RegionalAdmin from "./pages/RegionalAdmin/RegionalAdmin.jsx";
 
 function App() {
   const location = useLocation();
@@ -31,6 +31,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/category" element={<Category />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" />
         <Route path="/login" />
         <Route path="/offers" element={<Offers />} />
         <Route path="/seller" element={<SellerDashboard />} />
